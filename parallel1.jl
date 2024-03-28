7b419ee59389e52d90c167b5486f4246aecf8932
MengqianXu <xumengqian201808@163.com>
2024-03-28 01:16:42 +0100

    Update parallel1.jl

--- parallel1.jl
+++ parallel1.jl
@@ -3,18 +3,15 @@ using MPI
 using LinearAlgebra
 using SparseArrays
 using Random
+using Plots
 
-MPI.Init()
-comm = MPI.COMM_WORLD
-rank = MPI.Comm_rank(comm)
-size = MPI.Comm_size(comm)
 
 function Multigrid(A, F, N::Int64, x0, pre, post)
     if N == 1
         return (1/4)*F
     else
         U = copy(x0)
-
+        #Distributed memory parallel computing
         @sync begin
             @distributed for i = 1:pre
                 U = parallel_jacobi(A, F, U)
@@ -51,13 +48,15 @@ function Multigrid(A, F, N::Int64, x0, pre, post)
     end
 end
 
-
+#At the end of each iteration, use the MPI.Allreduce function to summarize the nouveau vectors of all processes, 
+#add their results, and store the result back to nouveau
 
 function parallel_jacobi(A, b, x0, tol = 1e-15, max_iters = 1000)
     ancien = copy(x0)
     nouveau = copy(b)
     n = length(b)
     iter = 0
+    temp = zeros(length(ancien))
     while iter < max_iters
         iter += 1
         for i = 1:n
@@ -69,37 +68,36 @@ function parallel_jacobi(A, b, x0, tol = 1e-15, max_iters = 1000)
             end
             nouveau[i] = (b[i] - sum) / A[i, i]
         end
-        
-        
-        temp = MPI.Allreduce(nouveau,  MPI.SUM, comm)
-        copy!(ancien, temp)
-        
+        temp = MPI.Allreduce(nouveau, MPI.SUM, comm)
+        nouveau = copy(temp) 
         if norm(nouveau - ancien) < tol
             break
         end
     end
     
     return nouveau
+
 end
 
+
+    
+
+MPI.Initialized()
+comm = MPI.COMM_WORLD
+rank = MPI.Comm_rank(comm)
+size = MPI.Comm_size(comm)
 N = 3
 A = Creer_A(N)
 F1 = Creer_F(p, q, N)
-F2 = Creer_F(α, β, γ, δ, N)
 U1 = [u(p, q, i/(N + 1), j /(N + 1)) for j = 1:N, i = 1:N][:]
-U2 = [u(α, β, γ, δ, i/(N + 1), j /(N + 1)) for j = 1:N, i = 1:N][:]
 x0 = rand(1:9, (N*N, 1))
-
 tabY = [i/(N + 1) for i = 1:N, j = 1:N][:]
 tabX = [j/(N + 1) for i = 1:N, j = 1:N][:]
-
 sol1 = A\F1
-
-resM1 = Multigrid(A, F1, N, x0, 1, 1)
+@benchmark begin
+    resM1 = Multigrid(A, F1, N, x0, 1, 1)
+end
+println(resM1)
 p1 = plot(tabX, tabY, [U1, sol1,  resM1], label = ["Solution exacte" "Directe" "Multigrid"])
-
-sol2 = A\F2
-resM2 = Multigrid(A, F2, N, x0, 1, 1)
-p2 = plot(tabX, tabY, [U2, sol2,resM2], label = ["Solution exacte" "Directe" "Multigrid"])
 
 MPI.Finalize()
