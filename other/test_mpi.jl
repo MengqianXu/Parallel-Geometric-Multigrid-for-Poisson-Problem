d2ae4152ebfef8d6d5c27852821e2744188f635f
MengqianXu <xumengqian201808@163.com>
2024-04-24 01:59:39 +0200

    Create test_mpi.jl

---
+++ other/test_mpi.jl
@@ -0,0 +1,18 @@
+using Distributed
+using MPI
+using LinearAlgebra
+# using BenchmarkTools
+
+function test()
+    println("hhhh")
+end
+
+MPI.Init()
+comm = MPI.COMM_WORLD
+rank = MPI.Comm_rank(comm)
+size = MPI.Comm_size(comm)
+# @benchmark begin
+test()
+# end
+
+MPI.Finalize()
