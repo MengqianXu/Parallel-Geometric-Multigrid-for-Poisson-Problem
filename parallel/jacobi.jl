33f58756ab4bd1fa18ede99c0aa22df99fed97da
MengqianXu <xumengqian201808@163.com>
2024-04-24 00:47:33 +0200

    Update jacobi.jl

--- parallel/jacobi.jl
+++ parallel/jacobi.jl
@@ -1,8 +1,7 @@
-using Pkg
-Pkg.add("MPI")
 using MPI
 
 MPI.Init()
+
 function calculate_subdomain(rank, npx, npy, N)
     myidx = rank % npx
     myidy = div(rank, npx)
@@ -26,7 +25,8 @@ function parallel_jacobi(A, b, N, npx, npy, tol=1e-15, max_iter=10000)
     size = MPI.Comm_size(comm)
     x0, y0, nlx, nly = calculate_subdomain(rank, npx, npy, N)
     @assert 1 <= x0 <= N && 1 <= y0 <= N && (x0 + nlx - 1) <= N && (y0 + nly - 1) <= N
-    
+    nlx = Int(nlx)
+    nly = Int(nly)
     # Extract local matrix A and vector b
     local_A = A[x0:x0+nlx-1, y0:y0+nly-1]
     local_b = b[x0:x0+nlx-1]
@@ -38,7 +38,6 @@ function parallel_jacobi(A, b, N, npx, npy, tol=1e-15, max_iter=10000)
     norm_diff = Inf
     
     while norm_diff > tol && iteration < max_iter
-       
         new_local_u .= local_u
         for i = 2:nlx-1
             for j = 2:nly-1
@@ -54,16 +53,21 @@ function parallel_jacobi(A, b, N, npx, npy, tol=1e-15, max_iter=10000)
         local_u .= new_local_u
         
         # Send and receive boundary data, blocking communication
-        # 交换边界数据
         if x0 > 1
             dest_rank = rank - 1
             MPI.send(local_u[2, :], dest_rank, 0, comm)
+            println("rank $dest_rank for sending boundary data.")
             MPI.recv!(local_u[1, :], dest_rank, 0, comm)
         end
         if x0 + nlx - 1 < N
             dest_rank = rank + 1
-            MPI.send(local_u[nlx-1, :], dest_rank, 0, comm)
-            MPI.recv!(local_u[nlx, :], dest_rank, 0, comm)
+            if dest_rank < size
+                MPI.send(local_u[nlx-1, :], dest_rank, 0, comm)
+                MPI.recv!(local_u[nlx, :], dest_rank, 0, comm)
+            else
+                println("Error: Invalid destination rank $dest_rank for sending boundary data.")
+                break
+            end
         end
         if y0 > 1
             dest_rank = rank - npx
@@ -83,7 +87,11 @@ function parallel_jacobi(A, b, N, npx, npy, tol=1e-15, max_iter=10000)
     end
     
     # Collect local solution vectors of all processes to the main process
-    gathered_local_u = MPI.Gather(local_u[2:end-1, 2:end-1], 0, comm)
+    if size > 1
+        gathered_local_u = MPI.Gather(local_u[2:end-1, 2:end-1], 0, comm)
+    else
+        gathered_local_u = [local_u[2:end-1, 2:end-1]]  
+    end
     
     if rank == 0
         # The main process splices all local solution vectors to obtain the final result vector
@@ -117,7 +125,7 @@ function benchmark_jacobi_methods(N)
     println("Final result vector size: $(length(x_parallel_jacobi))")
     println("parallel Jacobi",x_parallel_jacobi)
     # Compare solutions
-    println("difference between solutions:", maximum(abs.(x_jacobi - x_parallel_jacobi)))
+    #println("difference between solutions:", maximum(abs.(x_jacobi - x_parallel_jacobi)))
 end
 
 
